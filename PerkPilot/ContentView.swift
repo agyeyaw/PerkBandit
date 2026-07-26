9cd42fcbfa6a7b0951ba30396cfd7b48e2e8aefa
Yaw Agyemang <agyemangy73@gmail.com>
2026-07-26 15:54:56 -0400

    Initial Commit

---
+++ PerkPilot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PerkPilot
+//
+//  Created by Yaw Agyemang on 7/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PerkPilot/PerkPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerkPilotApp.swift
+//  PerkPilot
+//
+//  Created by Yaw Agyemang on 7/26/26.
+//
+
+import SwiftUI
+
+@main
+struct PerkPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
